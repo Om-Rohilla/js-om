a05bf45eea34fc7e4284f81c367a9cdc07b9b59f
Om-Rohilla <omrohilla106@gmail.com>
2024-12-01 10:27:27 +0000

    added

---
+++ 01_basic/datatype.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert("hello be aware");   we are using node js not browser
+
+
+let Name = "Om rohilla";
+let age = 18
+let state;
+
+
+//number {2 to the power 34}
+//Bigint  { for large nnumber }
+//string  {for asny alphabetic charactor}
+//boolen "true/ false"
+//null "Standalone value"
+//undefined =>
+//symbol  => object
+
+// object
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof null);
---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+const accountID = 455
+let accountEmail  = "om@gmail.com"
+var accountPass = "java1"
+accountCity = "haryana"
+let accountState;
+
+// accountID = 2 (not allowed)
+// accountCity = "gurgoan"   (can be changed)
+// accountEmail = "new@gmail.com"  (can be change)
+// accountPass = "ok1"  (can be changed )
+
+//  console.log(accountCity);
+
+//another method of console is by Using table
+console.table([accountEmail, accountID, accountPass, accountCity, accountState]);
